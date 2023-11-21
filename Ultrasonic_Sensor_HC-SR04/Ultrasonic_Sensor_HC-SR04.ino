0e3c3d730a17363429ca0b693a539588dd87af0d
Paula Olivan <55576202+PaulaOlivan@users.noreply.github.com>
2023-11-21 16:09:51 +0200

    Intruder system on ultrasonic sensor

--- Ultrasonic_Sensor_HC-SR04/Ultrasonic_Sensor_HC-SR04.ino
+++ Ultrasonic_Sensor_HC-SR04/Ultrasonic_Sensor_HC-SR04.ino
@@ -1,6 +1,8 @@
 const int triggerPin = 9;
 const int echoPin = 10;
 float pulse_width, distance;
+float wall;
+float range = 5.0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -9,6 +11,17 @@ void setup() {
   Serial.begin(9600);
   digitalWrite(triggerPin, LOW);
   delayMicroseconds(2);
+
+  // Distance to the wall
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  pulse_width = pulseIn(echoPin, HIGH);
+  wall = (pulse_width*.0343)/2; //measure in cm, divided by 2 cause sounds goes and comes back
+  Serial.print("Distance to the wall = ");
+  Serial.print(wall);
+  Serial.println(" cm");
+  delay(500);
 }
 
 void loop() {
@@ -18,8 +31,15 @@ void loop() {
   digitalWrite(triggerPin, LOW);
   pulse_width = pulseIn(echoPin, HIGH);
   distance = (pulse_width*.0343)/2; //measure in cm, divided by 2 cause sounds goes and comes back
-  Serial.print("Distance = ");
-  Serial.println(distance);
-  Serial.println(" cm");
-  delay(500);
+  if (distance > (wall+range) || distance < (wall-range)){
+    Serial.print("Intruder detected at = ");
+    Serial.print(distance);
+    Serial.println(" cm");
+    wall = distance;
+    Serial.print("New wall measure = ");
+    Serial.print(wall);
+    Serial.println(" cm");
+  }
+
+  delay(3000);
 }
