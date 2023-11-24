06eecac6741c1d9689ffa53be3f2edca5e7035d4
Paula Olivan <771938@unizar.es>
2023-11-24 11:05:32 +0200

    Infrared Final Demo

--- Infrared_Sensor_GP2DI2/Infrared_Sensor_GP2DI2.ino
+++ Infrared_Sensor_GP2DI2/Infrared_Sensor_GP2DI2.ino
@@ -1,38 +1,85 @@
 /*SHARP GP2Y0A21YK0F IR distance sensor with Arduino and SharpIR library example code. More info: https://www.makerguides.com */
 
 // Include the library:
-#include 
+//#include SharpIR-master
 
-// Define model and input pin:
-#define IRPin A0
-#define model 1080
+// Wait a minute until take the value of the if sensor, we will take 5 values and compute the mean in the variable if_reference
+// If this value change up or down 50 the alarm will detected intrusion
 
-// Create variable to store the distance:
-int distance_cm;
-
-/* Model :
-  GP2Y0A02YK0F --> 20150
-  GP2Y0A21YK0F --> 1080
-  GP2Y0A710K0F --> 100500
-  GP2YA41SK0F --> 430
-*/
-
-// Create a new instance of the SharpIR class:
-SharpIR mySensor = SharpIR(IRPin, model);
+float GP2D12; //Used to be a char
+float a,b; // Used to be a char
+byte if_pin = A0;
+float wall;
+bool first = true;
+int range = 10;
 
 void setup() {
-  // Begin serial communication at a baudrate of 9600:
-  Serial.begin(9600);
+  Serial.begin(9600); //
 }
 
-void loop() {
-  // Get a distance measurement and store it as distance_cm:
-  distance_cm = mySensor.distance();
+void loop(){
 
-  // Print the measured distance to the serial monitor:
-  Serial.print("Mean distance: ");
-  Serial.print(distance_cm);
-  Serial.println(" cm");
+  if (first){
+    delay (10000);
+    int mean;
+    mean = if_mean();
+    Serial.print("Mean value: ");
+    Serial.println(mean);
 
-  delay(1000);
+    a = mean / 10;
+    b = mean % 10;
+    wall = a*10 + b;
+
+    if(wall>10 && wall<80){
+      Serial.print(a,DEC); 
+      Serial.print(b,DEC);
+      Serial.println("cm"); 
+    }
+  }
+
+  int val;
+  GP2D12 = read_gp2d12_range(if_pin);
+  Serial.print("Value: ");
+  Serial.println(GP2D12);
+  a=GP2D12/10;
+  b=GP2D12%10;
+  val=a*10+b;
+
+  if (val>10&&val<80){
+    Serial.print(a,DEC);//
+    Serial.print(b,DEC);//
+    Serial.println("cm");//
+    if (val > (wall+range) || val < (wall+range)){
+      Serial.println ("Intruder detected");
+    } 
+  }
+
+  else{
+    Serial.println("Outside range");//
+  }
+
+  delay(3000);
+}
+
+float if_mean(){
+  int sum;
+  for (i=0; i<5; i++){
+    sum = sum + read_gp2d12_range(if_pin);
+  }
+  delay(1000); //Wait 1 sec before next reading
+  return (sum/5);
+}
+
+
+float read_gp2d12_range(byte pin){
+  int tmp;
+  tmp = analogRead(pin);
+  Serial.print("tmp value: ");
+  Serial.println(tmp);
+  int res;
+  res = (6787.0 /((float)tmp - 3.0)) - 4.0;
+  Serial.print("Result value: ");
+  Serial.println(res);
+  if (tmp < 3)return -1;
+  return res;
 }
